0ea3b0bdc2c1b0dd6898e728f14f1f06456565ad
Jenna <116000928+jnnhan@users.noreply.github.com>
2023-05-11 18:23:44 +0300

    Check the operating system to make the app Windows compatible

--- tasks.py
+++ tasks.py
@@ -1,33 +1,37 @@
 from invoke import task
+import os
 
-@task
-def read_sudokus(ctx):
-    ctx.run("python3 src/read_sudokus.py", pty=True)
+def _islinux():
+    """Check if operating system is Linux. 
+        This is done in order to make the app Windows compatible.
+    """
+
+    return os.sys.platform.startswith('linux')
 
 @task()
 def start(ctx):
-    ctx.run("python3 src/index.py", pty=True)
+    ctx.run("python3 src/index.py", pty=_islinux())
 
-@task(post=[read_sudokus])
+@task()
 def init(ctx):
-    ctx.run("python3 src/init_database.py", pty=True)
+    ctx.run("python3 src/init_database.py", pty=_islinux())
 
 @task
 def test(ctx):
-    ctx.run("pytest src", pty=True)
+    ctx.run("pytest src", pty=_islinux())
 
 @task
 def lint(ctx):
-    ctx.run("pylint src", pty=True)
+    ctx.run("pylint src", pty=_islinux())
 
 @task
 def format(ctx):
-    ctx.run("autopep8 --in-place --recursive src", pty=True)
+    ctx.run("autopep8 --in-place --recursive src", pty=_islinux())
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest src", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=_islinux())
 
 @task(coverage)
 def coverage_report(ctx):
-    ctx.run("coverage html", pty=True)
+    ctx.run("coverage html", pty=_islinux())
