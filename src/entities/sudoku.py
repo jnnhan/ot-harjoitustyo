910cf2c3ef84ae608a269c9e712d68312edd9d67
Jenna <116000928+jnnhan@users.noreply.github.com>
2023-05-13 23:51:24 +0300

    Add docstring

--- src/entities/sudoku.py
+++ src/entities/sudoku.py
@@ -1,5 +1,20 @@
 class Sudoku:
-    def __init__(self, name, puzzle, level):
+    """Class for a single sudoku.
+
+    Attributes:
+        name: (string) Sudoku's name 
+        puzzle: (String) Sudoku's numbers in a string format
+        level: (Int) Sudoku's level   
+    """
+
+    def __init__(self, name=None, puzzle=None, level=None):
+        """Initialize the class for making a new sudoku.
+
+        Args:
+            name: Sudoku's name. Defaults to None.
+            puzzle: Sudoku's numbers. Defaults to None.
+            level: Level of the sudoku. Defaults to None.
+        """
         self.name = name
         self.puzzle = puzzle
         self.level = level
--- src/entities/user.py
+++ src/entities/user.py
@@ -1,4 +1,17 @@
 class User:
+    """Class for a single user.
+    
+        Attributes:
+            username: Username for the user
+            password: Password for the user
+    """
+
     def __init__(self, username, password):
+        """Initialize the class for creating a new user.
+
+        Args:
+            username: Username for the user
+            password: password for the user
+        """
         self.username = username
         self.password = password
